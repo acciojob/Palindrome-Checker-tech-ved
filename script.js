58802838a8492b7d2ba51331963aa8efa4b2850b
Vedant Dubey <112743896+tech-ved@users.noreply.github.com>
2026-02-14 23:32:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
-// complete the given function
-
-function palindrome(str){
-
+function isPalindrome(s) {
+    let cleaned = s.toLowerCase().replace(/[^a-z0-9]/g, "");
+    let reversed = cleaned.split("").reverse().join("");
+    return cleaned === reversed;
 }
-module.exports = palindrome
